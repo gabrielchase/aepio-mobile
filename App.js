16ba8729d0e25c88b4c122b7e82366ab645cb5c6
gabrielchase <gchase.patron@gmail.com>
2018-05-06 15:47:44 +0800

    Fix `AuthReducer` import and take out unneeded import

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
 
 import reducers from './reducers';
