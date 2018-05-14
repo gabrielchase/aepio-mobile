8fd0861556d4def276dd7f9014e4c943a3c38dc0
gabrielchase <gchase.patron@gmail.com>
2018-05-14 15:11:43 +0800

    Add Edit Form to the router

--- Router.js
+++ Router.js
@@ -5,6 +5,7 @@ import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
 import DeviceList from './components/DeviceList'
 import DeviceDetail from './components/DeviceDetail'
+import EditForm from './components/EditForm'
 
 
 const RouterComponent = () => {
@@ -16,8 +17,9 @@ const RouterComponent = () => {
                     <Scene key="register" component={RegisterForm} title="Registration" />
                 </Stack>
                 <Stack key="main">
-                    <Scene key="deviceList" component={DeviceList} title="Device List" initial/>
+                    <Scene key="deviceList" component={DeviceList} title="Device List" initial />
                     <Scene key="deviceDetail" component={DeviceDetail} title="Device" />
+                    <Scene key="userEditForm" component={EditForm} title="Update Details" />
                 </Stack>
             </Stack>
         </Router>
